38c6352049f8f4aefdd9f64628fc13ed6c49035d
JoseLuisSebastian <jlsebastianavila@gmail.com>
2020-08-23 10:22:20 +0000

    generate all the possible domain

---
+++ generate-domine/app.js
@@ -0,0 +1,14 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big'];
+let noun = ['jogger','racoon'];
+
+
+for (let indexPronoun =0; indexPronoun <= pronoun.length - 1; indexPronoun++) {
+    for (let indexAdj =0; indexAdj <= adj.length - 1; indexAdj++) {
+        for (let indexNoun =0; indexNoun <= noun.length - 1; indexNoun++) {
+
+                console.log(pronoun[indexPronoun]+adj[indexAdj]+ noun[indexNoun] + ".com");
+
+        }
+    }
+}
