0ae9cd6c09f045f378ba7141716a311da6b65043
JoseLuisSebastian <jlsebastianavila@gmail.com>
2020-08-23 11:11:18 +0000

    added some information

--- generate-domine/app.js
+++ generate-domine/app.js
@@ -1,3 +1,6 @@
+//Create a script that generate all the possible domain name 
+//combinations from a list of pronouns, adjectives and nouns
+
 let pronoun = ['the','our'];
 let adj = ['great', 'big'];
 let noun = ['jogger','racoon'];
